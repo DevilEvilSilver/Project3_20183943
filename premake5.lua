d182e9efbd6eda9a69e945151f5af281b86b325e
DevilEvilSilver <longtgm1142@gmail.com>
2021-09-12 19:28:38 +0700

    +add precompiled headers

--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,9 @@ project "Silver"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+	pchheader "pch.h"
+	pchsource "Silver/src/pch.cpp"
+
 	files
 	{
 		"%{prj.name}/src/**.h",
